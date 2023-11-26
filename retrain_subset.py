b769f3633eb5f56db71fc5fa559c60294f60a189
weiliao <liaoweithu@gmail.com>
2023-11-26 20:09:14 +0000

    retrain mlp

--- retrain_subset.py
+++ retrain_subset.py
@@ -186,104 +186,105 @@ if __name__ == "__main__":
     trainval_ids = train_id + dev_id
 
     for c_fold, (train_index, test_index) in enumerate(kf.split(trainval_head)):
-        best_loss = 1e4
-        patience = 0
-        if c_fold >= 1:
-            model.load_state_dict(torch.load('/content/start_weights.pt'))
-        print('Starting Fold %d' % c_fold)
-        print("TRAIN:", len(train_index), "TEST:", len(test_index))
-        train_head, val_head = utils.slice_data(trainval_head, train_index), utils.slice_data(trainval_head, test_index)
-        train_static, val_static = utils.slice_data(trainval_static, train_index), utils.slice_data(trainval_static, test_index)
-        train_stail, val_stail = utils.slice_data(trainval_stail, train_index), utils.slice_data(trainval_stail, test_index)
-        train_id, val_id = utils.slice_data(trainval_ids, train_index), utils.slice_data(trainval_ids, test_index)
+        if c_fold == 0: 
+            best_loss = 1e4
+            patience = 0
+            if c_fold >= 1:
+                model.load_state_dict(torch.load('/content/start_weights.pt'))
+            print('Starting Fold %d' % c_fold)
+            print("TRAIN:", len(train_index), "TEST:", len(test_index))
+            train_head, val_head = utils.slice_data(trainval_head, train_index), utils.slice_data(trainval_head, test_index)
+            train_static, val_static = utils.slice_data(trainval_static, train_index), utils.slice_data(trainval_static, test_index)
+            train_stail, val_stail = utils.slice_data(trainval_stail, train_index), utils.slice_data(trainval_stail, test_index)
+            train_id, val_id = utils.slice_data(trainval_ids, train_index), utils.slice_data(trainval_ids, test_index)
 
-        train_dataloader, dev_dataloader, test_dataloader = prepare_retrain.get_data_loader(args, train_head, val_head,
-                                                                                            test_head, 
-                                                                                            train_stail, val_stail,
-                                                                                            test_sofa,
-                                                                                            train_static=train_static,
-                                                                                            dev_static=val_static,
-                                                                                            test_static=test_static,
-                                                                                            train_id=train_id,
-                                                                                            dev_id=val_id,
-                                                                                            test_id=test_id)
+            train_dataloader, dev_dataloader, test_dataloader = prepare_retrain.get_data_loader(args, train_head, val_head,
+                                                                                                test_head, 
+                                                                                                train_stail, val_stail,
+                                                                                                test_sofa,
+                                                                                                train_static=train_static,
+                                                                                                dev_static=val_static,
+                                                                                                test_static=test_static,
+                                                                                                train_id=train_id,
+                                                                                                dev_id=val_id,
+                                                                                                test_id=test_id)
 
-        for j in range(args.epochs):
-            model.train()
-            sofa_list = []
-            sofap_list = []
-            loss_t = []
-            loss_to = []
+            for j in range(args.epochs):
+                model.train()
+                sofa_list = []
+                sofap_list = []
+                loss_t = []
+                loss_to = []
 
-            for vitals, static, target, train_ids, key_mask in train_dataloader:
-                # print(label.shape)
-                if args.warmup == True:
-                    model_opt.optimizer.zero_grad()
-                else:
-                    model_opt.zero_grad()
-                # ti_data = Variable(ti.float().to(device))
-                # td_data = vitals.to(device) # (6, 182, 24)
-                # sofa = target.to(device)
-       
-                if args.model_name == 'TCN': 
-                    sofa_p = model(vitals.to(device))
-                elif args.model_name == 'RNN':
-                    # x_lengths have to be a 1d tensor
-                    td_transpose = vitals.to(device).transpose(1, 2)
-                    x_lengths = torch.LongTensor([len(key_mask[i][key_mask[i] == 0]) for i in range(key_mask.shape[0])])
-                    sofa_p = model(td_transpose, x_lengths)
-                elif args.model_name == 'Transformer':
-                    tgt_mask = model.get_tgt_mask(vitals.to(device).shape[-1]).to(device)
-                    sofa_p = model(vitals.to(device), tgt_mask, key_mask.bool().to(device))
-       
-
-                loss = utils.mse_maskloss(sofa_p, target.to(device), key_mask.to(device))
-                loss.backward()
-                model_opt.step()
-
-                sofa_list.append(target)
-                sofap_list.append(sofa_p)
-                loss_t.append(loss)
-
-            loss_avg = np.mean(torch.stack(loss_t, dim=0).cpu().detach().numpy())
-
-            model.eval()
-            y_list = []
-            y_pred_list = []
-            ti_list = []
-            td_list = []
-            id_list = []
-            loss_val = []
-            with torch.no_grad():  # validation does not require gradient
-
-                for vitals, static, target, val_ids, key_mask in dev_dataloader:
-
-                    if args.model_name == 'TCN':
-                        sofap_t = model(vitals.to(device))
+                for vitals, static, target, train_ids, key_mask in train_dataloader:
+                    # print(label.shape)
+                    if args.warmup == True:
+                        model_opt.optimizer.zero_grad()
+                    else:
+                        model_opt.zero_grad()
+                    # ti_data = Variable(ti.float().to(device))
+                    # td_data = vitals.to(device) # (6, 182, 24)
+                    # sofa = target.to(device)
+        
+                    if args.model_name == 'TCN': 
+                        sofa_p = model(vitals.to(device))
                     elif args.model_name == 'RNN':
-                        # x_lengths have to be a 1d tensor 
+                        # x_lengths have to be a 1d tensor
                         td_transpose = vitals.to(device).transpose(1, 2)
                         x_lengths = torch.LongTensor([len(key_mask[i][key_mask[i] == 0]) for i in range(key_mask.shape[0])])
-                        sofap_t = model(td_transpose, x_lengths)
+                        sofa_p = model(td_transpose, x_lengths)
                     elif args.model_name == 'Transformer':
                         tgt_mask = model.get_tgt_mask(vitals.to(device).shape[-1]).to(device)
-                        sofap_t = model(vitals.to(device), tgt_mask, key_mask.bool().to(device))
+                        sofa_p = model(vitals.to(device), tgt_mask, key_mask.bool().to(device))
+        
 
-                    loss_v = utils.mse_maskloss(sofap_t, target.to(device), key_mask.to(device))
-                    y_list.append(target.detach().numpy())
-                    y_pred_list.append(sofap_t.cpu().detach().numpy())
-                    loss_val.append(loss_v)
-                    id_list.append(val_ids)
+                    loss = utils.mse_maskloss(sofa_p, target.to(device), key_mask.to(device))
+                    loss.backward()
+                    model_opt.step()
 
-            loss_te = np.mean(torch.stack(loss_val, dim=0).cpu().detach().numpy())
-            if loss_te < best_loss:
-                patience = 0
-                best_loss = loss_te
-                torch.save(model.state_dict(),
-                            base + workname + '/' + 'fold%d' % c_fold + '_best_loss.pt')
-            else:
-                patience += 1
-                if patience >= 10:
-                    print('Start next fold')
-                    break
-            print('Epoch %d, : Train loss is %.4f, test loss is %.4f' % (j, loss_avg, loss_te))
+                    sofa_list.append(target)
+                    sofap_list.append(sofa_p)
+                    loss_t.append(loss)
+
+                loss_avg = np.mean(torch.stack(loss_t, dim=0).cpu().detach().numpy())
+
+                model.eval()
+                y_list = []
+                y_pred_list = []
+                ti_list = []
+                td_list = []
+                id_list = []
+                loss_val = []
+                with torch.no_grad():  # validation does not require gradient
+
+                    for vitals, static, target, val_ids, key_mask in dev_dataloader:
+
+                        if args.model_name == 'TCN':
+                            sofap_t = model(vitals.to(device))
+                        elif args.model_name == 'RNN':
+                            # x_lengths have to be a 1d tensor 
+                            td_transpose = vitals.to(device).transpose(1, 2)
+                            x_lengths = torch.LongTensor([len(key_mask[i][key_mask[i] == 0]) for i in range(key_mask.shape[0])])
+                            sofap_t = model(td_transpose, x_lengths)
+                        elif args.model_name == 'Transformer':
+                            tgt_mask = model.get_tgt_mask(vitals.to(device).shape[-1]).to(device)
+                            sofap_t = model(vitals.to(device), tgt_mask, key_mask.bool().to(device))
+
+                        loss_v = utils.mse_maskloss(sofap_t, target.to(device), key_mask.to(device))
+                        y_list.append(target.detach().numpy())
+                        y_pred_list.append(sofap_t.cpu().detach().numpy())
+                        loss_val.append(loss_v)
+                        id_list.append(val_ids)
+
+                loss_te = np.mean(torch.stack(loss_val, dim=0).cpu().detach().numpy())
+                if loss_te < best_loss:
+                    patience = 0
+                    best_loss = loss_te
+                    torch.save(model.state_dict(),
+                                base + workname + '/' + 'fold%d' % c_fold + '_best_loss.pt')
+                else:
+                    patience += 1
+                    if patience >= 10:
+                        print('Start next fold')
+                        break
+                print('Epoch %d, : Train loss is %.4f, test loss is %.4f' % (j, loss_avg, loss_te))
